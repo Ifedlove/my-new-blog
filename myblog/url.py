36144d17183987a3d846b0351ceeae34bd51df0f
Ifeoluwa <dloveade@gmail.com>
2022-04-21 11:06:46 +0100

    my last commit for the project

--- myblog/url.py
+++ myblog/url.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('post_list.html/', views.post_list, name='post_list'),
+
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
